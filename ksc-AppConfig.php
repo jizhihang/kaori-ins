19b5a1533028b44cbb6949aab227c5484787edc6
ledduy@gmail.com <ledduy@gmail.com>
2013-08-14 13:47:05 +0900

    Quantization with kmeams Num cluster = 4K!

--- ksc-AppConfig.php
+++ ksc-AppConfig.php
@@ -139,7 +139,7 @@ $gnMaxFrameHeight = 500; // *** CHANGED ***
 $gszResizeOption = sprintf("-resize '%sx%s>'", $gnMaxFrameWidth, $gnMaxFrameHeight); // to ensure W is the width after shrinking
 
 /// !!! IMPORTANT PARAM !!!
-$nNumClusters = 500;
+$nNumClusters = 4000;
 $szKMeansMethod = 'elkan';
 
 $szTrialName = sprintf("Soft-%d", $nNumClusters);
