a307d46d6bb02235cf86c0c20b32c12cc4b1b980
ledduy@gmail.com <ledduy@gmail.com>
2014-07-31 17:56:41 +0900

    Minor update
    Modify and add comments!
    Not checking bug-free!

--- nvtiep/extract_hesaffine_rootsift_noangle.m
+++ nvtiep/extract_hesaffine_rootsift_noangle.m
@@ -2,8 +2,14 @@ function extract_hesaffine_rootsift_noangle(DB, startShotInd, endShotInd)
 % example run
 % extract_hesaff_rootsift_noangle('INS2013', 1, 1)
 
+% IMPORTANT NOTE
+% Du lieu duoc luu truc tiep len server dir --> next version se chinh lai de luu tren local tmp cua tung node, sau khi finish se upload len server dir
+% dung binary hesaffine --> du lieu luu ra file trung gian --> convert ve format dung chung nhu perdoch
+
 switch DB
 case 'INS2013'
+	% ds cac shotID - tao ra bang cach dung lenh >> ls /net/per610a/export/das11g/caizhizhu/ins/ins2013/frames_png
+	
 	lst_shots_file = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/INS/INS2013/meta/lst_shots.mat';
 	db_frame_dir = '/net/per610a/export/das11g/caizhizhu/ins/ins2013/frames_png';
 	db_feat_dir = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/INS/INS2013/hesaff_rootsift_noangle_mat';
@@ -12,12 +18,38 @@ case 'INS2013'
 	end
 end
 
-exe = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/tool/compute_descriptors_64bit.ln';
-run('/net/per610a/export/das11f/ledduy/plsang/nvtiep/libs/vlfeat-0.9.18/toolbox/vl_setup.m');
+DB = 'INS2013';
+switch DB
+case 'INS2013'
+    work_dir = fullfile('/net/per610a/export/das11f/ledduy/plsang/nvtiep/INS/',DB);
+end
 
 feature_config = '-hesaff -rootsift -noangle';
 feature_dir = strrep(feature_config, '-', '');
 feature_dir = strrep(feature_dir, ' ', '_');
+
+%	lst_shots_file = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/INS/INS2013/meta/lst_shots.mat';
+%	db_frame_dir = '/net/per610a/export/das11g/caizhizhu/ins/ins2013/frames_png';
+%	db_feat_dir = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/INS/INS2013/hesaff_rootsift_noangle_mat';
+
+% ds cac shotID - tao ra bang cach dung lenh >> ls /net/per610a/export/das11g/caizhizhu/ins/ins2013/frames_png
+lst_shots_file = fullfile(work_dir, '/meta/lst_shots.mat');
+
+% keyframe dir - imported from CZ
+db_frame_dir = '/net/per610a/export/das11g/caizhizhu/ins/ins2013/frames_png';
+
+% output feature dir
+db_feat_dir = fullfile(work_dir,[feature_name '_mat']); 
+
+if ~exist(db_feat_dir, 'dir')
+	mkdir(db_feat_dir);
+end
+
+% code nay thuong cho ket qua tot hon so voi perdoch
+exe = '/net/per610a/export/das11f/ledduy/plsang/nvtiep/tool/compute_descriptors_64bit.ln'; 
+run('/net/per610a/export/das11f/ledduy/plsang/nvtiep/libs/vlfeat-0.9.18/toolbox/vl_setup.m');
+
+% tmp dir la can thiet
 tmp_dir = '/tmp';
 sift_dir = fullfile(tmp_dir, feature_dir);
 if ~exist(sift_dir, 'dir')
@@ -26,15 +58,18 @@ end
 
 % open list shot file
 load(lst_shots_file);
-test_ids = cellfun(@(x) isempty(strfind(x, 'shot0_')), lst_shots, 'UniformOutput', false);
-lst_shots = lst_shots(cell2mat(test_ids));
 nshot = length(lst_shots);
 
+
 %matlabpool('open', 8);
+% startShot & endShot dung de chay tren grid
 for i=startShotInd:endShotInd
 	fprintf('\r %d - %d - %d', i, startShotInd, endShotInd);
 	shot_name = lst_shots{i};
 	shot_frame_dir = fullfile(db_frame_dir, shot_name);
+	
+	% bug co kha nang xay ra neu shot_feature_file ko phai unique khi chay tren grid, 
+	% doi voi INS13 - shotID la duy nhat
 	shot_feature_file = fullfile(db_feat_dir, [shot_name,'.mat']);
 	if exist(shot_feature_file, 'file') && ~renew
 		continue;
