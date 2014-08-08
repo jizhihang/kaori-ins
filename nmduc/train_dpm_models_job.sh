dea724e8d02a2f57fb421f5ac819593a079592a1
ledduy@gmail.com <ledduy@gmail.com>
2014-08-08 10:29:00 +0900

    Update - runnable version
    Ko hiểu vì sao lệnh này để ngoài matlab lại gây ra lỗi init của matlab
    nên phải đưa vào trong: #cd
    /net/per900c/raid0/ledduy/github-projects/kaori-ins2014/voc-release5

--- nmduc/train_dpm_models_job.sh
+++ nmduc/train_dpm_models_job.sh
@@ -4,7 +4,7 @@
 # Force to use shell sh. Note that #$ is SGE command
 #$ -S /bin/sh
 # Force to limit hosts running jobs
-#$ -q all.q@bc501.hpc.vpl.nii.ac.jp,all.q@bc502.hpc.vpl.nii.ac.jp,all.q@bc503.hpc.vpl.nii.ac.jp
+#$ -q all.q@@bc5hosts
 # Log starting time
 date 
 # include your library here
@@ -12,11 +12,12 @@ export LD_LIBRARY_PATH=/net/per610a/export/das09f/satoh-lab/minhduc/dependencies
 # display your command here
 echo [$HOSTNAME] [$JOB_ID] [matlab -nodisplay -r "train_model( 'query_$1' )"]
 # change to your code directory here
-cd /net/per900c/raid0/ledduy/github-projects/kaori-ins2014/voc-release5
+#cd /net/per900c/raid0/ledduy/github-projects/kaori-ins2014/voc-release5
 # Log info of current dir
 pwd
 # run your command with parameters ($1, $2,...) here, string variable is put in ' '
-matlab -nodisplay -r "addpath('/net/per610a/export/das11f/ledduy/trecvid-ins-2014/model/ins-dpm/tv2013/query2013/$1'); global VOC_CONFIG_OVERRIDE; VOC_CONFIG_OVERRIDE = @voc_config_$1;  train_model('query_$1', 1);"
+
+matlab -nodisplay -r "cd /net/per900c/raid0/ledduy/github-projects/kaori-ins2014/voc-release5; addpath('/net/per610a/export/das11f/ledduy/trecvid-ins-2014/model/ins-dpm/tv2013/query2013/$1'); global VOC_CONFIG_OVERRIDE; VOC_CONFIG_OVERRIDE = @voc_config_$1;  train_model('query_$1', 1);"
 # Log ending time
 date
 
